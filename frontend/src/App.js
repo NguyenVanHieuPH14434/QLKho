444e300ee556cdd5946aeb731d2a179dfff05bc9
rogerkieu <kieuthaian0212@gmail.com>
2022-09-13 17:54:26 +0700

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import Header from "./Shared/Header/header";
 import Transaction from "./components/Transaction/transaction";
 import Producer from "./components/Producer/producer";
-import Lot from "./components/Lot/lot";
+import Consignment from "./components/Consignment/Consignment";
 import Pharmacy from "./components/Pharmarcy/pharmacy";
 import Management from "./components/Management/management";
-import { Routes, Route } from 'react-router-dom';
-import './App.scss';
+import { Routes, Route } from "react-router-dom";
+import "./App.scss";
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
         <Route path="/" element={<Producer />}></Route>
         <Route path="transaction" element={<Transaction />}></Route>
         <Route path="producer" element={<Producer />}></Route>
-        <Route path="lot" element={<Lot />}></Route>
+        <Route path="consignment" element={<Consignment />}></Route>
         <Route path="pharmacy" element={<Pharmacy />}></Route>
         <Route path="management" element={<Management />}></Route>
       </Routes>
